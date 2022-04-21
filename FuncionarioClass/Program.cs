e68edbef68895046ddd4b5b72332c0983fbb4cb7
Geison Amorim <geisonam43@gmail.com>
2022-04-20 21:48:05 -0300

    Add project files.

---
+++ FuncionarioClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario F = new Funcionario();
+
+            Console.WriteLine("Dados do Funcionario: ");
+            Console.Write("\nNome: ");
+            F.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            F.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            F.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: "+F);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite a porcentagem para aumentar o salário:");
+            double qte = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            F.AumentarSalario(qte);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + F);
+        }
+    }
+}
